688ac23f1962889095358ca6283fd1baf4b97647
kushalkumarghosh <ghoshkushalcse@gmail.com>
2024-05-31 12:09:39 +0600

    updated the logo and asset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
-                <img src={logo} alt="logo image" style={{ width: '80px' }} />
+                <img src={logo} alt="logo image" style={{ width: '100px' }} />
             </div>
 
             {/* menu */}
