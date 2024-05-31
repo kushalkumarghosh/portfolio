3f106191cc92fe7d6e2059424524fe23a8990eeb
kushalkumarghosh <ghoshkushalcse@gmail.com>
2024-05-31 20:31:23 +0600

    finished the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,21 @@ const About = () => {
                         <p className='text-4xl font-bold inline border-b-4 border-[#ffbd59]'>About</p>
                     </div>
                     <div>
-
                     </div>
                 </div>
-            </div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>Hi. I'm Kushal, nice to meet you. Please take a look around. </p>
+                    </div>
+                    <div>
+                        <p>As a visionary Front-End Developer, I bring digital dreams to life with flair and precision. I specialize in creating immersive, user-centric web experiences that are as functional as they are beautiful. Let's transform the ordinary into the extraordinary.</p>
+                    </div>
 
+                </div>
+            </div>
         </div>
+
+
     );
 };
 
