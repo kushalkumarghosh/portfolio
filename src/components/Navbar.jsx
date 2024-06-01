c544920db1947c70d20be20d405f80214a89c2a4
kushalkumarghosh <ghoshkushalcse@gmail.com>
2024-06-01 22:47:41 +0600

    updated the contact and work section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,22 +19,22 @@ const Navbar = () => {
 
             <ul className='hidden md:flex'>
                 <li>
-                    <Link to='home' smooth={true} duration={500}>
+                    <Link to='home' smooth={true} duration={500} >
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link to='about' smooth={true} duration={500}>
+                    <Link to='about' smooth={true} duration={500} >
                         About
                     </Link>
                 </li>
                 <li>
-                    <Link to='skills' smooth={true} duration={500}>
+                    <Link to='skills' smooth={true} duration={500} >
                         Skills
                     </Link>
                 </li>
                 <li>
-                    <Link to='work' smooth={true} duration={500}>
+                    <Link to='work' smooth={true} duration={500} >
                         Work
                     </Link>
                 </li>
