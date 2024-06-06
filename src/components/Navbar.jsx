5868a5435e956d3a69c5a1fb5ec17356b8e14304
kushalkumarghosh <ghoshkushalcse@gmail.com>
2024-06-06 22:10:44 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-custom-bg bg-noise-texture text-gray-300'>
             <div>
-                <img src={logo} alt="logo image" style={{ width: '100px' }} />
+                {/* <img src={logo} alt="logo image" style={{ width: '100px' }} /> */}
             </div>
 
             {/* menu */}
