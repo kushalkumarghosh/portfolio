86183aa1bb6c9f25e42bf476d3909f98bd6913be
kushalkumarghosh <ghoshkushalcse@gmail.com>
2025-08-10 22:36:30 +0600

    ipdated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,8 @@
 import workImg from "../assets/projects/RR.jpg";
 import workImg1 from "../assets/projects/Blog1.jpg";
 import workImg2 from "../assets/projects/eatery-cafe.png";
+import workImg3 from "../assets/projects/envo.png";
+import workImg4 from "../assets/projects/portfolio.png";
 
 const Work = () => {
   return (
@@ -95,6 +97,64 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/kushalkumarghosh/eatery-cafe"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${workImg3})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React.js Website
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://envobyte-delta.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kushalkumarghosh/envobyte"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${workImg4})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React.js Website
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://portfolio-delta-five-0jbk4wh8vm.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kushalkumarghosh/portfolio"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
